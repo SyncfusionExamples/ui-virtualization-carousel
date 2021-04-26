c306d5b5333b3385251bd9d07e50f05819502213
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 12:59:26 +0530

    UIvirtualization sample added for carousel

---
+++ UIVirtualization/UIVirtualization/ViewModel/CarouselViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIVirtualization
+{
+    public class CarouselViewModel
+    {
+        private List<CarouselModel> imageCollection = new List<CarouselModel>();
+        public List<CarouselModel> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value; }
+        }
+        public CarouselViewModel()
+        {
+            for (int i = 1; i < 25; i++)
+            {
+                ImageCollection.Add(new CarouselModel("Image" + i + ".jpg"));
+            }
+
+        }
+    }
+}
